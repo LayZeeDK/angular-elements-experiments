5fbb3c38eead38d31e1a86141025ac94b549a342
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-03-28 21:16:19 +0200

    feat: add minimal elements without platform and NgZone app

---
+++ apps/minimal-elements-without-platform-and-ngzone/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'minimal-elements-without-platform-and-ngzone'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('minimal-elements-without-platform-and-ngzone');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('minimal-elements-without-platform-and-ngzone app is running!');
+  });
+});
