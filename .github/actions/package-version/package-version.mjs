0c9f2b96aa7c6dff3919ca544c69c5d0d1fea44f
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-04-01 00:12:04 +0200

    feat: implement package-version action

--- .github/actions/package-version/package-version.mjs
+++ .github/actions/package-version/package-version.mjs
@@ -1,4 +1,21 @@
-// Read package.json
-// Locate package
-// Change package version
-// Write to package.json
+import { readFileSync, writeFileSync } from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+function updatePackageVersion(name, version) {
+  // Read package.json
+  const packageJsonPath = path.resolve(__dirname, "../../../package.json");
+  const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
+  // Locate package
+  // Change package version
+  packageJson.dependencies[name] = version;
+  // Write to package.json
+  writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+}
+
+// Not available in an ES Module as of Node.js 12.x
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const name = process.argv[2];
+const version = process.argv[3];
+
+updatePackageVersion(name, version);
